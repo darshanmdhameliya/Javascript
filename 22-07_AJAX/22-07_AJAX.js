3588edb955e11676790caba21b7932d246b7c994
darshandhameliya000 <skillqodedarshan@gmail.com>
2024-07-22 20:33:49 +0530

    22-07_AJAX

--- 22-07_AJAX/22-07_AJAX.js
+++ 22-07_AJAX/22-07_AJAX.js
@@ -1,35 +1,35 @@
 /* javascript AJAX */
 
-{
-  var https = new XMLHttpRequest();
+// {
+//   var https = new XMLHttpRequest();
 
-  https.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-      console.log(this.responseText);
-    } else {
-      console.log("Page Not Found!");
-    }
-  };
+//   https.onreadystatechange = function () {
+//     if (this.readyState == 4 && this.status == 200) {
+//       console.log(this.responseText);
+//     } else {
+//       console.log("Page Not Found!");
+//     }
+//   };
 
-  https.open("GET", "./input.txt", true);
-  https.send();
-}
+//   https.open("GET", "./input.txt", true);
+//   https.send();
+// }
 
-{
-  var https = new XMLHttpRequest();
+// {
+//   var https = new XMLHttpRequest();
 
-  https.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-      console.log(this.responseText);
-    }
-    if (this.readyState == 4 && this.status == 404) {
-      console.log("Page Not Found!");
-    }
-  };
+//   https.onreadystatechange = function () {
+//     if (this.readyState == 4 && this.status == 200) {
+//       console.log(this.responseText);
+//     }
+//     if (this.readyState == 4 && this.status == 404) {
+//       console.log("Page Not Found!");
+//     }
+//   };
 
-  https.open("GET", "./input.txt", true);
-  https.send();
-}
+//   https.open("GET", "./input.txt", true);
+//   https.send();
+// }
 
 /* javascript Function Method */
 
@@ -41,19 +41,63 @@
 // apply(thisArg)
 // apply(thisArg, argsArray)
 
-{
-    let profile = {
-        firstname:'darshan',
-        lastname:'dhameliya'
-    }
+// {
+//   let profile = {
+//     firstname: 'darshan',
+//     lastname: 'dhameliya'
+//   }
 
 
-    function profileapply(a,b){
-        console.log(`${a}=${b}  ${this.firstname} = ${this.lastname}`);
-    }
+//   function profileapply(a, b) {
+//     console.log(`${a}=${b}  ${this.firstname} = ${this.lastname}`);
+//   }
 
-    let Funcapply = profileapply.apply(profile, ['hi','how are you!'])
+//   let Funcapply = profileapply.apply(profile, ['hi', 'how are you!'])
 
-    console.log(Funcapply);
-}
+//   console.log(Funcapply);
+// }
 
+/* call method */
+
+// {
+//   let profile = {
+//     firstname: 'darshan',
+//     lastname: 'dhameliya'
+//   }
+
+
+//   function profileapply(a, b) {
+//     console.log(`${this.firstname}=${this.lastname}  ${a}=${b} `);
+//   }
+
+//   let Funcapply = profileapply.call(profile, 'hi', 'how are you!')
+
+//   console.log(Funcapply);
+// }
+
+/* Bind method */
+
+// {
+//   const student1 = {
+//     name: "janvi",
+//     grade: "5",
+//     itroduction: function () {
+//       console.log(this.name + " student in grade " + this.grade + ".");
+//     },
+//   };
+
+//   const student2 = {
+//     name: "darshan",
+//     grade: "6",
+//   }
+
+//   const student3 = {
+//     name: "sweety",
+//     grade: "12",
+//   }
+
+//   let result = student1.itroduction.bind(student2)
+
+//   result();
+
+// }
