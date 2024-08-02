2c62355abaa7c55313882170441f9b58a582b4af
Dhaval Solanki <dhavalas24@gmail.com>
2024-08-02 18:00:14 +0530

    prectice

---
+++ prectice/index.js
@@ -0,0 +1,23 @@
+// (function (){
+//     let a =" hello darshan";
+// })();
+
+// {
+//   function myFunction(a, b) {
+//     return a * b;
+//     let a1= "hello darshan!!"
+//   }
+//   let text = myFunction.toString();
+//   console.log(text);
+
+// }
+
+{
+ let a= myFunction(5);
+ console.log(a);
+ 
+  function myFunction(y) {
+    return y * y;
+  }
+
+}
